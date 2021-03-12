0463f66315fbdba6b85e5df7ef9dd2f0c4455ff8
hokula <adelhokula18@gmail.com>
2021-03-12 17:34:02 +0100

    Escape Sequences in Strings

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,6 +1,6 @@
 function main() {
-var myString = "FirstLine\n\t\\SecondLine\nThirdLine"
-return myString;
+    var myString = "FirstLine\n\t\\SecondLine\nThirdLine";
+    return myString;
 }
 console.log(main());
 module.exports = main;
